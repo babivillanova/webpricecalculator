c4138abc83cc23ce70467c8c300346d96c9f02c8
babivillanova <77370233+babivillanova@users.noreply.github.com>
2022-05-06 22:01:03 -0300

    .

--- src/calculator.js
+++ src/calculator.js
@@ -1,53 +1,55 @@
 // Grab the things I need in the DOM
 const priceInput = document.querySelector("[name=price]");
-const quantityInput = document.querySelector("[name=quantity]");
 const quartosInput = document.querySelector("[name=quartos]");
+const quartos2Input = document.querySelector("[name=quartos2]");
 const banhosInput = document.querySelector("[name=banhos]");
 
 const cidadeInput = document.querySelector("[name=cidade]");
 //total
 const total = document.querySelector(".total");
 //labels
-const quantityLabel = document.querySelector(".quantity-label");
 const quartosLabel = document.querySelector(".quartos-label");
+const quartos2Label = document.querySelector(".quartos2-label");
 const banhosLabel = document.querySelector(".banhos-label");
 
-cidadeInputValue = cidadeInput.value;
 
-if(cidadeInputValue == "São Paulo"){
-    cidade = 1
-}else if(cidadeInputValue == "Florianópolis"){
-    cidade = 2
-}else if(cidadeInputValue == "Belo Horizonte"){
-    cidade = 3}
 
 
 
 // Write the function here
 function calculateCost() {
+
+    const cidadeInputValue = cidadeInput.value;
+
+    if(cidadeInputValue == "São Paulo"){
+            cidade = 2
+        }else if(cidadeInputValue == "Florianópolis"){
+            cidade = 2
+        }else if(cidadeInputValue == "Belo Horizonte"){
+            cidade = 3}
      
     const price = priceInput.value;
-    const quantity = quantityInput.value;
     const quartos = quartosInput.value;
+    const quartos2 = quartos2Input.value;
     const banhos = banhosInput.value;
     
-    const cost = quantity * (quartos*4750) + (banhos*6000);
+    const cost = (quartos2*4750) + (quartos*4750) + (banhos*6000) + (price*1) * (1*cidade);
+
     total.innerText = "R$" + cost.toFixed(2);
-
 }
 
 
 
 
-
-function updateQuantityLabel(_argument) {
-    const quantity = quantityInput.value;
-    quantityLabel.innerText = quantity;
-}
 
 function updateQuartosLabel(_argument) {
     const quartos = quartosInput.value;
     quartosLabel.innerText = quartos;
+}
+
+function updateQuartos2Label(_argument) {
+    const quartos2 = quartos2Input.value;
+    quartos2Label.innerText = quartos2Input;
 }
 
 function updateBanhosLabel(_argument) {
@@ -60,11 +62,12 @@ calculateCost();
 
 // Add Events Listeners
 priceInput.addEventListener("input", calculateCost);
-quantityInput.addEventListener("input", calculateCost);
-quantityInput.addEventListener("input", updateQuantityLabel);
 quartosInput.addEventListener("input", updateQuartosLabel);
 quartosInput.addEventListener("input", calculateCost);
+
+quartos2Input.addEventListener("input", calculateCost);
+quartos2Input.addEventListener("input", updateQuartos2Label);
 banhosInput.addEventListener("input", updateBanhosLabel);
 banhosInput.addEventListener("input", calculateCost);
-cidadeInput.addEventListener("selectcidade", calculateCost);
+cidadeInput.addEventListener("select", calculateCost); //precisa corrigir o eventListener
 
